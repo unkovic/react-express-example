7c3be323fd9cb802764f920439d6ce84e19e6154
unkovic <unkovic.nemanja@yahoo.com>
2015-11-19 23:04:46 +0100

    Initial commit

---
+++ app/main.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+console.log("Hello from JSX!");
+
+var GroceryItemList = require("./components/GroceryItemList.jsx");
+
+ReactDOM.render(<GroceryItemList />, app);
